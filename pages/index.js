be2950e89103cb2cd37cd5c331e2ccfe90c39996
jason <2B3>
2021-09-29 00:46:51 -0400

    static regen B

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,17 @@ import React, { useEffect, useRef, useState } from "react";
 import classes from "./index.module.css";
 import { putFB, getFB } from "../helpers/httpRequests";
 
-function index() {
-  const [display, setDisplay] = useState(0);
+function index(props) {
   const inputRef = useRef();
-  useEffect(() => {
-    get();
-  }, []);
-  const get = async () => {
-    const parsedResponse = await getFB();
-    setDisplay(parsedResponse);
-  };
   const putHandler = async () => {
     const inputVal = inputRef.current.value;
     await putFB({ 1: inputVal }); // needs a ref
     inputRef.current.value = "";
   };
-
   return (
     <section className={classes.section}>
       <h1>Current data on the backend:</h1>
-      <p>{display}</p>
+      <p>{props.display}</p>
       <div>
         <button onClick={putHandler}>PUT on backend</button>
         <input ref={inputRef} placeholder="Will be sent to Firebase..."></input>
@@ -29,5 +20,12 @@ function index() {
     </section>
   );
 }
-
 export default index;
+
+export async function getStaticProps(){
+  let responseData= await getFB()
+  return { 
+  	props: { display: responseData }, 
+  	revalidate: 2 // refresh data every 60 seconds, 
+  };
+}
